c41d540bfe8285155edcbc981a5401f19604011f
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-15 21:28:56 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function(e) {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function(e) {
+    $('img').addClass('tasty');
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert("Your form is going to be submitted now.");
+  });
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    if(e.which === 71) {
+      window.alert('Gee whiz!');
+    }
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  submitIt();
+  pressIt();
 });
